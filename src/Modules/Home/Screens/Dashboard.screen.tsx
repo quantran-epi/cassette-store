365647ac46aa7a84267246690a59533d1fd8b419
quantd <quan.td@shb.com.vn>
2025-07-04 12:17:35 +0700

    update

--- src/Modules/Home/Screens/Dashboard.screen.tsx
+++ src/Modules/Home/Screens/Dashboard.screen.tsx
@@ -18,11 +18,18 @@ import {ArrowUpOutlined, ArrowDownOutlined} from "@ant-design/icons";
 import {useSelector} from "react-redux";
 import {RootState} from "@store/Store";
 import {Col, Row} from "@components/Grid";
+import {List} from "@components/List";
+import {OrderItemWidget} from "@modules/Order/Screens/OrderItem/OrderItem.widget";
+import {orderBy, sortBy} from "lodash";
+import {Space} from "@components/Layout/Space";
+import {Typography} from "@components/Typography";
+import {Tag} from "@components/Tag";
 
 moment.updateLocale('en', {week: {dow: 1}});
 
 export const DashboardScreen = () => {
     const orders = useSelector((state: RootState) => state.order.orders);
+    const customers = useSelector((state: RootState) => state.customer.customers);
     const {} = useScreenTitle({value: "Thống kê", deps: []});
     const orderUtils = useOrder();
 
@@ -88,19 +95,21 @@ export const DashboardScreen = () => {
         <br/>
         <Card bordered={false} title={"Bom"}>
             <Stack fullwidth direction={"column"} align={"flex-start"}>
-                <Statistic
-                    title="Số đơn"
-                    value={orders.filter(e => e.status === ORDER_STATUS.RETURNED && e.returnReason === ORDER_RETURN_REASON.REFUSE_TO_RECEIVE).length}
-                    suffix="đơn"
-                    valueStyle={{color: COLORS.ORDER_STATUS.RETURNED}}
-                />
-                <Statistic
-                    title="Số băng"
-                    value={orders.filter(e => e.status === ORDER_STATUS.RETURNED && e.returnReason === ORDER_RETURN_REASON.REFUSE_TO_RECEIVE)
-                        .reduce((prev, cur) => prev + cur.placedItems.reduce((prev1, cur1) => prev1 + cur1.count, 0), 0)}
-                    suffix="băng"
-                    valueStyle={{color: COLORS.ORDER_STATUS.RETURNED}}
-                />
+                <Stack fullwidth justify={"space-between"}>
+                    <Statistic
+                        title="Số đơn"
+                        value={orders.filter(e => e.status === ORDER_STATUS.RETURNED && e.returnReason === ORDER_RETURN_REASON.REFUSE_TO_RECEIVE).length}
+                        suffix=""
+                        valueStyle={{color: COLORS.ORDER_STATUS.RETURNED}}
+                    />
+                    <Statistic
+                        title="Số băng"
+                        value={orders.filter(e => e.status === ORDER_STATUS.RETURNED && e.returnReason === ORDER_RETURN_REASON.REFUSE_TO_RECEIVE)
+                            .reduce((prev, cur) => prev + cur.placedItems.reduce((prev1, cur1) => prev1 + cur1.count, 0), 0)}
+                        suffix=""
+                        valueStyle={{color: COLORS.ORDER_STATUS.RETURNED}}
+                    />
+                </Stack>
                 <Statistic
                     title="Tiền ship"
                     value={orders.filter(e => e.status === ORDER_STATUS.RETURNED && e.returnReason === ORDER_RETURN_REASON.REFUSE_TO_RECEIVE)
@@ -121,6 +130,50 @@ export const DashboardScreen = () => {
                     valueStyle={{color: COLORS.ORDER_STATUS.SHIPPED}}
                 />)}
             </Stack>
+        </Card>
+        <Divider>Thống kê khách hàng</Divider>
+        <Card bordered={false} title={"Khách mua lại"}>
+            <Stack fullwidth direction={"column"} align={"flex-start"}>
+                <Statistic
+                    title="Mua lại lần 2"
+                    value={customers.filter(c => c.buyCount == 2).length}
+                    suffix=""
+                    valueStyle={{color: COLORS.CUSTOMER.CONFIRMED}}
+                />
+                <Statistic
+                    title="Mua lại 3 lần trở lên"
+                    value={customers.filter(c => c.buyCount > 2).length}
+                    suffix=""
+                    valueStyle={{color: COLORS.CUSTOMER.CONFIRMED}}
+                />
+                <Statistic
+                    title="VIP"
+                    value={customers.filter(c => c.isVIP).length}
+                    suffix=""
+                    valueStyle={{color: COLORS.CUSTOMER.VIP}}
+                />
+                <Statistic
+                    title="Bom"
+                    value={customers.filter(c => c.isInBlacklist).length}
+                    suffix=""
+                    valueStyle={{color: COLORS.CUSTOMER.BLACK_LIST}}
+                />
+            </Stack>
+        </Card>
+        <Card bordered={false} title={"Top 10"}>
+            <List
+                pagination={false}
+                itemLayout="horizontal"
+                dataSource={orderBy(customers, ['buyAmount'], ['desc']).slice(0, 9)}
+                renderItem={(item, index) => <Space size={5}>
+                    <Space size={3}>
+                        <Typography.Text>{index + 1}. {item.name}</Typography.Text>
+                        {item.isVIP && <Tag color={COLORS.CUSTOMER.VIP}>VIP</Tag>}
+                    </Space>
+                    <Tag>{item.buyCount} đơn</Tag>
+                    <Tag>{item.buyAmount}đ</Tag>
+                </Space>}
+            />
         </Card>
     </React.Fragment>
 }
