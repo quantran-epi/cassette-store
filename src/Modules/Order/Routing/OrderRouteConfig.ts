807bf39a8dcf8b916de9e51b6f68c85cacfa1b3a
Duc Quan Tran <quantd0811@gmail.com>
2026-06-16 20:22:55 +0700

    feat(03-01): rename order route config symbol
    - Rename the local order route config from CustomerRoutes to OrderRoutes
    
    - Preserve /order/list, /order/create, and /order/cod-payment-list paths

--- src/Modules/Order/Routing/OrderRouteConfig.ts
+++ src/Modules/Order/Routing/OrderRouteConfig.ts
@@ -1,9 +1,9 @@
 import { RouteHelpers } from "@common/Helpers/RouteHelper"
 
-const CustomerRoutes = RouteHelpers.CreateRoutes('/order', (orderRoot) => ({
+const OrderRoutes = RouteHelpers.CreateRoutes('/order', (orderRoot) => ({
     List: () => RouteHelpers.CreateRoute(orderRoot, ["list"]),
     Create: () => RouteHelpers.CreateRoute(orderRoot, ["create"]),
     CodPaymentList: () => RouteHelpers.CreateRoute(orderRoot, ["cod-payment-list"])
 }))
 
-export default CustomerRoutes
+export default OrderRoutes
