6bae34e07866d005f49e59396a30d10653198e11
Duc Quan Tran <quantd0811@gmail.com>
2026-06-16 20:23:32 +0700

    feat(03-01): mount order routes with OrderRouter
    - Import the existing OrderRouter layout
    
    - Use OrderRouter for the /order branch
    
    - Leave the customer branch on CustomerRouter

--- src/Routing/RootRouter.tsx
+++ src/Routing/RootRouter.tsx
@@ -9,13 +9,14 @@ import {DashboardScreen} from "@modules/Home/Screens/Dashboard.screen";
 import {OrderCreateScreen} from "@modules/Order/Screens/OrderCreate/OrderCreate.screen";
 import {OrderListScreen} from "@modules/Order/Screens/OrderList.screen";
 import {OrderCodPaymentListScreen} from "@modules/Order/Screens/OrderCodPayment/OrderCodPaymentList.screen";
+import {OrderRouter} from "@modules/Order/Routing/OrderRouter";
 
 export const RootRouter = () => {
     return <BrowserRouter basename="/cassette-store">
         <Routes>
             <Route path={RootRoutes.AuthorizedRoutes.Root()} element={<MasterPage />}>
                 <Route index element={<DashboardScreen />} />
-                <Route path={RootRoutes.AuthorizedRoutes.OrderRoutes.Root()} element={<CustomerRouter />}>
+                <Route path={RootRoutes.AuthorizedRoutes.OrderRoutes.Root()} element={<OrderRouter />}>
                     <Route path={RootRoutes.AuthorizedRoutes.OrderRoutes.List()} element={<OrderListScreen />} />
                     <Route path={RootRoutes.AuthorizedRoutes.OrderRoutes.Create()} element={<OrderCreateScreen />} />
                     <Route path={RootRoutes.AuthorizedRoutes.OrderRoutes.CodPaymentList()} element={<OrderCodPaymentListScreen />} />
